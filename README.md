ca3857d57039510579f055ccfe8f5660f131c0b8
Gettor <sdobroc@gmail.com>
2018-03-17 18:00:33 +0100

    Banana

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Initial commit
+
+Colors:
+Strong skill (red): #F42516
+Swift skill (green): #2B8C2F
+Mystic skill (blue): #258DEA
+Basic background (dark grey) : #383838
+Skill disabled (light grey) : #AAAAAA
---
+++ myscripts.js
@@ -0,0 +1,70 @@
+const iconPrefix = "icons/";
+const enabledSkills = [
+    iconPrefix + "strongAtkActive.png",
+    iconPrefix + "swiftAtkActive.png",
+    iconPrefix + "mysticAtkActive.png",
+    iconPrefix + "strongDefActive.png",
+    iconPrefix + "swiftDefActive.png",
+    iconPrefix + "mysticDefActive.png"
+];
+
+const disabledSkills = [
+    iconPrefix + "strongAtkDisabled.png",
+    iconPrefix + "swiftAtkDisabled.png",
+    iconPrefix + "mysticAtkDisabled.png",
+    iconPrefix + "strongDefDisabled.png",
+    iconPrefix + "swiftDefDisabled.png",
+    iconPrefix + "mysticDefDisabled.png"
+];
+
+function setBackgroundImages() {
+    var skills = getSkills();
+    for (var i = 0; i < skills.length; i++) {
+        if (skills[i].disabled)
+        {
+            skills[i].style.backgroundImage = "url(" + disabledSkills[i] + ")";
+        }
+        else
+        {
+            skills[i].style.backgroundImage = "url(" + enabledSkills[i] + ")";
+        }
+    }
+}
+
+function getSkills() {
+    var a1 = document.getElementById("a1");
+    var a2 = document.getElementById("a2");
+    var a3 = document.getElementById("a3");
+    var d1 = document.getElementById("d1");
+    var d2 = document.getElementById("d2");
+    var d3 = document.getElementById("d3");
+    return [a1, a2, a3, d1, d2, d3];
+}
+
+function onClick() {
+    var ph1 = document.getElementsByClassName("ph1");
+    var ph2 = document.getElementsByClassName("ph2");
+    var fight = document.getElementsByClassName("fight");
+    var startBtn = document.getElementsByClassName("startButton");
+
+    ph1[0].style.visibility = "visible";
+    ph2[0].style.visibility = "visible";
+    fight[0].style.visibility = "visible";
+    startBtn[0].disabled = true;
+    startBtn[0].className = "duelInProgressButton";
+}
+
+function toggleSkill(skillId) {
+    var skills = getSkills();
+    skills[0].disabled ^= true;
+    skills[1].disabled ^= true;
+    skills[2].disabled ^= true;
+    skills[3].disabled ^= true;
+    skills[4].disabled ^= true;
+    skills[5].disabled ^= true;
+    setBackgroundImages();
+}
+
+window.onload = function() {
+  setBackgroundImages();
+};
